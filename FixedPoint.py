2a5921f1abaab968b38822883e3108c17ceb1b01
rickycs97 <rickycs97@pku.edu.cn>
2019-11-06 22:24:22 +0800

    Create FixedPoint.py

---
+++ FixedPoint.py
@@ -0,0 +1,23 @@
+"""Fixed Point Iteration"""
+import numpy as np
+import math
+def F(x):
+    return x**3 + x - 1
+
+def g(x):
+    return (1-x)**(1/3)
+x0 = 0.5
+err = 0.0005
+
+x0 = 0.5
+err = 0.00005
+#x = x0
+
+
+for i in range(2000):
+    x = g(x0)
+    if math.fabs(x - x0) < err:
+        print(x,"iteration {}".format(i))
+        break
+    else:
+        x0 = x
